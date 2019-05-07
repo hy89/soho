7362c9212a5e5c96a710f89855cbd9bafb7ac06e
heyin <hycj89@163.com>
2019-05-07 13:23:35 +0800

    删除cookies隐私

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 cookies = {
-    "DCF_SESS": "1mzQ2xsTQeFjv0_7tGZ_ijqqIOE3PwdsrcWc7_namVNesnNZ8LkCTQSi65v-G5Kr",
+    "DCF_SESS": "xxx-G5Kr",
 }
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
